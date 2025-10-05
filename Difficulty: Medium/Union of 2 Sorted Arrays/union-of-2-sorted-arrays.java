cdfa800e35fb71721f3bbb26c66a0435d8a24c93
Bhumika Bindal <bhumikabindal60@gmail.com>
2025-10-05 21:17:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
@@ -0,0 +1,60 @@
+class Solution {
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        // code here
+        ArrayList <Integer> union= new ArrayList<>();
+        int i=0,j=0;
+        int n1=a.length;
+        int n2=b.length;
+        while(i<n1&&j<n2){
+            if(a[i]<b[j]){
+                if(union.isEmpty()||union.get(union.size()-1)!=a[i]){
+                    union.add(a[i]);
+                    
+                    
+                }i++;
+            }
+        
+        
+             else if(a[i]>b[j]){
+                if(union.isEmpty()||union.get(union.size()-1)!=b[j]){
+                    union.add(b[j]);
+                
+                    
+                }j++;
+            }
+        
+             else{//a[i]==b[j]
+                if(union.isEmpty()||union.get(union.size()-1)!=a[i]){
+                    union.add(a[i]);
+                   
+                    
+                } i++;
+                    j++;
+            }
+        
+        
+        
+        
+    }
+    while(i<n1){
+         if(union.isEmpty()||union.get(union.size()-1)!=a[i]){
+             union.add(a[i]);
+
+         }
+                     i++;
+    }
+         
+    
+        while(j<n2){
+         if(union.isEmpty()||union.get(union.size()-1)!=b[j]){
+             union.add(b[j]);
+
+         }
+                         j++;
+                         
+        }
+         return union;
+         
+    }
+    
+}
