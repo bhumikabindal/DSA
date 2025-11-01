fb47e72afb7ea94376c28d3c98e9f1675ff6c2fb
Bhumika Bindal <bhumikabindal60@gmail.com>
2025-11-01 14:26:34 +0530

    Time: 2 ms (99.08%) | Memory: 47.2 MB (5.28%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> mpp = new HashMap<>();
+
+     
+
+        // 2️⃣ Second pass — check for remainder
+        for (int i = 0; i < nums.length; i++) {
+            int rem = target - nums[i];
+            if (mpp.containsKey(rem) ) {
+                return new int[]{i, mpp.get(rem)};
+            }
+            mpp.put(nums[i],i);
+        }
+
+        return new int[]{};
+    }}
